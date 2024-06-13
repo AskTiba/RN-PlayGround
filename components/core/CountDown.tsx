ea1e57d928e618bb7e97e6cdf936e7785741a346
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-06-13 17:57:57 +0300

    Update CountDown.tsx

--- components/core/CountDown.tsx
+++ components/core/CountDown.tsx
@@ -1,5 +1,6 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
 import React, { useEffect, useState } from "react";
+import { BlurView } from "expo-blur";
 
 // Define the prop types
 interface CountdownProps {
@@ -42,32 +43,48 @@ const CountDown = ({ targetDate }: CountdownProps) => {
   }, [targetDate]);
 
   return (
-    <View className=" mt-4 mx-6 flex bg-[#32cd32]/[0.85] flex-row justify-center items-center rounded-lg divide-white divide-x-[1px]">
-      <View className="flex w-[25%] justify-center items-center px-3 py-[1px]">
-        <Text className="text-gray-700 font-extrabold uppercase text-[12px]">
-          Days
-        </Text>
-        <Text className="text-white text-[18px] font-[900]">{time.days}</Text>
-      </View>
-      <View className="flex w-[24%] justify-center items-center px-3 py-1">
-        <Text className="text-gray-700 font-extrabold uppercase text-[12px]">
-          Hours
-        </Text>
-        <Text className=" text-white text-[18px] font-[900]">{time.hours}</Text>
-      </View>
-      <View className="flex w-[24%] justify-center items-center px-2 py-1">
-        <Text className="text-gray-700 font-extrabold uppercase text-[12px]">
-          Minutes
-        </Text>
-        <Text className=" text-white text-[18px] font-[900]">{time.minutes}</Text>
-      </View>
-      <View className="flex w-[25%] justify-center items-center px-3 py-1">
-        <Text className="text-gray-700 font-extrabold uppercase text-[12px]">
-          Seconds
-        </Text>
-        <Text className="text-white text-[18px] font-[900]">{time.seconds}</Text>
-      </View>
-    </View>
+    <ImageBackground
+      className="h-full w-full flex-auto justify-end"
+      source={require("../../assets/images/elsa.png")}
+    >
+      <BlurView
+        tint="dark"
+        intensity={80}
+        className=" mb-[18%] mx-3 flex flex-row justify-center items-center 
+    rounded-md divide-white divide-x-[1px] bg-transparent overflow-hidden"
+      >
+        <View className="flex w-[25%] justify-center items-center px-3 py-[1px]">
+          <Text className="text-pink-600 font-black uppercase text-[12px]">
+            Days
+          </Text>
+          <Text className="text-white text-[20px] font-[900]">{time.days}</Text>
+        </View>
+        <View className="flex w-[24%] justify-center items-center px-3 py-1">
+          <Text className="text-pink-600 font-black uppercase text-[12px]">
+            Hours
+          </Text>
+          <Text className=" text-white text-[20px] font-[900]">
+            {time.hours}
+          </Text>
+        </View>
+        <View className="flex w-[24%] justify-center items-center px-2 py-1">
+          <Text className="text-pink-600 font-black uppercase text-[12px]">
+            Minutes
+          </Text>
+          <Text className=" text-white text-[20px] font-[900]">
+            {time.minutes}
+          </Text>
+        </View>
+        <View className="flex w-[25%] justify-center items-center px-3 py-1">
+          <Text className="text-pink-600 font-black uppercase text-[12px]">
+            Seconds
+          </Text>
+          <Text className="text-white text-[20px] font-[900]">
+            {time.seconds}
+          </Text>
+        </View>
+      </BlurView>
+    </ImageBackground>
   );
 };
 
